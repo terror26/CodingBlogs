21463de0ebfb4c68835392e47cd75d047410a60c
Kanishk Verma <kanishkverma@Kanishks-MacBook-Air.local>
2023-11-07 14:07:09 +0530

    About me fixed

--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -1,7 +1,20 @@
-import React, { Component } from 'react'
+import React, { useState } from "react"
+import { Document, Page } from "react-pdf"
 
-const aboutMe = () => {
-    return <div> working about ME</div>;
+function aboutMe() {
+  const [numPages, setNumPages] = useState(null)
+  const [pageNumber, setPageNumber] = useState(1)
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages)
+  }
+
+  return (
+    <iframe
+      src="https://docs.google.com/gview?url= https://github.com/terror26/Resume/blob/56f6fa6994a9d3f7cfd226f0aae4d39dd4e0e5b0/Kanishk_Resume_Final_2023%20(1).pdf &embedded=true"
+      style="width:100%; height:100%;"
+      frameborder="0"
+    ></iframe>
+  )
 }
-
-export default aboutMe;
+export default aboutMe
