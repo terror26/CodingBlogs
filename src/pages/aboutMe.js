538e37554339f2ad181ca35019c0163b31729ce3
Kanishk Verma <kanishkverma@Kanishks-MacBook-Air.local>
2023-11-07 18:01:44 +0530

    Resume with ifram added

--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 const aboutMe = (props) => {
   console.log(props);
     return (
-      <div>          
-        <iframe src="https://github.com/terror26/Resume/blob/main/Kanishk_Resume_Final_2023%20(1).pdf" height="100%" width="100%"/>         
-      </div>
+        <div class="ResumeDisplay">
+        <iframe src="https://drive.google.com/file/d/1Vj-PkQ-zNcXEziphtNss5C7Gb66Nadw6/view?usp=sharing"/>   
+        </div>      
     )
 }
 
