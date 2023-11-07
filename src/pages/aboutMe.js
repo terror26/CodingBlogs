ab1fc121d3202ac623b81818c6ddcaab61ae50f3
Kanishk Verma <kanishkverma@Kanishks-MacBook-Air.local>
2023-11-07 15:40:47 +0530

    about me fixed

--- src/pages/aboutMe.js
+++ src/pages/aboutMe.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 
-const aboutMe = () => {
-    return <div> working about ME</div>;
+const aboutMe = (props) => {
+  console.log(props);
+    return (
+      <div>          
+        <iframe src="https://github.com/terror26/Resume/blob/main/Kanishk_Resume_Final_2023%20(1).pdf" height="100%" width="100%"/>         
+      </div>
+    )
 }
 
 export default aboutMe;
